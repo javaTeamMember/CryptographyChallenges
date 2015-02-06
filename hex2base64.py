558b3e3637b9b7d25a9107194c4f87c56844a5df
Kristen Harris <harriskristendan@gmail.com>
2015-02-06 13:57:07 -0500

    Added comments detailing program information

---
+++ hex2base64.py
@@ -0,0 +1,8 @@
+#Author: Kristen Harris
+#Program Name: hex2base64.py
+#Program Language: Python
+#Description: Convert a hex string to base64
+#
+#
+#Store Hex as string value
+hex = 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
